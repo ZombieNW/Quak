25d079a97248ecd7d1958d9cd4574941c9df8b13
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-01-21 13:31:35 -0700

    Split code into modules

---
+++ util/regexes.js
@@ -0,0 +1,11 @@
+export default {
+  number: /\d+/,
+  paren: /[()]/g,
+  letter: /[a-z]/i,
+  whitespace: /\s/,
+  quote: /\"/,
+  newline: /\n/,
+  tab: /\t/,
+  comma: /,/,
+  semicolon: /;/,
+};
