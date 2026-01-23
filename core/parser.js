c3085de66068bdd14140d69188da1a55d4b8ab33
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-01-23 00:38:21 -0700

    Change parser to new grammar

--- core/parser.js
+++ core/parser.js
@@ -1,63 +1,106 @@
 export function parser(tokens) {
   let current = 0;
 
-  function walk() {
-    let token = tokens[current];
-    if (token.type === "number") {
-      current++;
+  function parsePrimary() {
+	let token = tokens[current];
 
-      return {
-        type: "NumberLiteral",
-        value: token.value,
-      };
-    }
+	if (token.type === "number") {
+	  current++;
 
-    if (token.type === "string") {
-      current++;
+	  return {
+		type: "NumberLiteral",
+		value: token.value,
+	  };
+	}
 
-      return {
-        type: "StringLiteral",
-        value: token.value,
-      };
-    }
+	if (token.type === "identifier") {
+	  current++;
 
-    if (token.type === "paren" && token.value === "(") {
-      token = tokens[++current];
+	  return {
+		type: "Identifier",
+		value: token.value,
+	  };
+	}
 
-      let node = {
-        type: "CallExpression",
-        name: token.value,
-        params: [],
-      };
+	if (token.type === "string") {
+	  current++;
 
-      token = tokens[++current];
+	  return {
+		type: "StringLiteral",
+		value: token.value,
+	  };
+	}
 
-      while (
-        token.type !== "paren" ||
-        (token.type === "paren" && token.value !== ")")
-      ) {
-        // we'll call the `walk` function which will return a `node` and we'll
-        // push it into our `node.params`.
-        node.params.push(walk());
-        token = tokens[current];
-      }
+	if (token.type === "paren" && token.value === "(") {
+	  current++;
 
-      current++;
+	  let node = parseExpression();
+	  token = tokens[current];
 
-      return node;
-    }
+	  if (token.type === "paren" && token.value === ")") {
+		return node;
+	  }
+	}
 
-    throw new TypeError(token.type);
+	throw new TypeError("Unexpected token: " + JSON.stringify(token));
   }
 
+  function parseCallExpression(callee) {
+	let args = [];
+
+	// Skip the opening parenthesis
+	current++;
+	
+	// Parse arguments if call is not immediately closed
+	if (tokens[current].type !== "paren" && tokens[current].value !== ")") {
+		// Eats an expression, and checks if there's another
+		args.push(parseExpression());
+
+		while (tokens[current].type === "comma") {
+		  current++;
+		  args.push(parseExpression());
+		}
+	}
+
+	if (tokens[current].type !== "paren" || tokens[current].value !== ")") {
+		throw new TypeError("Expected token: )");
+	}
+
+	// Skip the closing parenthesis
+	current++;
+
+	return {
+		type: "CallExpression",
+		callee: callee,
+		arguments: args,
+	}
+  }
+
+  function parseExpression() {
+	let node = parsePrimary();
+	if (node) console.log(node);
+
+	while (
+    	tokens[current] &&
+    	tokens[current].type === "paren" &&
+    	tokens[current].value === "("
+	) {
+		node = parseCallExpression(node);
+	}
+
+	return node;
+  }
+
+
   let ast = {
-    type: "Program",
-    body: [],
+	type: "Program",
+	body: [],
   };
 
   while (current < tokens.length) {
-    ast.body.push(walk());
+	ast.body.push(parseExpression());
   }
 
   return ast;
 }
+
