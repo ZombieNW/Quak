743435fa188d3ae2d94ad3c7bad7f63c73aacff0
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-02-02 13:26:57 -0700

    Async Evaluation

--- core/evaluator.js
+++ core/evaluator.js
@@ -1,21 +1,25 @@
-export function evaluate(ast, env) {
+export async function evaluate(ast, env) {
 	switch (ast.type) {
 		case 'Program':
 			// Evaluate each statement
-			ast.body.forEach((statement) => evaluate(statement, env));
-			return null;
+			for (const statement of ast.body) {
+				await evaluate(statement, env);
+			}
+			process.exit(0);
 		case 'NumberLiteral':
 		case 'StringLiteral':
 			return ast.value;
 		case 'Identifier':
 			return env[ast.name];
 		case 'VariableDeclaration':
-			return (env[ast.name] = evaluate(ast.value, env));
+			return (env[ast.name] = await evaluate(ast.value, env));
 		case 'CallExpression':
 			// Evaluate the callee
-			let callee = evaluate(ast.callee, env);
-			// Evaluate the arguments
-			let args = ast.arguments.map((arg) => evaluate(arg, env));
-			return callee(...args);
+			const callee = await evaluate(ast.callee, env);
+			const args = await Promise.all(
+				ast.arguments.map(async (arg) => await evaluate(arg, env))
+			);
+
+			return await callee(...args);
 	}
 }
